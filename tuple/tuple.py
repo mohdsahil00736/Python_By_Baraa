ca22cd03be3d36f5eaec7b417f39a9667d8e3cf5
Mohammad Sahil <mohdsahil00736@gmail.com>
2026-03-30 15:00:32 +0530

    Tuple

---
+++ tuple/tuple.py
@@ -0,0 +1,17 @@
+
+# Tuple 
+
+my_tuple = ('p', 'a', 'c', 'a')
+my_tuple2 = (8, 2, 5, 7, 5)
+
+print(my_tuple)  # ordered , allows duplicate 
+print(my_tuple[1])  # indexed
+
+
+# my_tuple2[2] = 10  # immutable 
+print(my_tuple2)
+
+# my_tuple.remove('a') 
+
+print(sorted(my_tuple))
+print(sorted(my_tuple2))
